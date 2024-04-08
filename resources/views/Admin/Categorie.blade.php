7be270af136f3c5c9454afdf90fb9e8b90b19053
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-04-08 14:44:32 +0200

    Update Categorie.blade.php view with new session alerts and form changes

--- resources/views/Admin/Categorie.blade.php
+++ resources/views/Admin/Categorie.blade.php
@@ -1,6 +1,20 @@
 @extends('Admin.DashboardAdmin')
   @section('content') 
 
+  @if(session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
+@if(session('danger'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('danger') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Add Categorie</button>
 
   
@@ -12,15 +26,14 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Categorie:</label>
-            <input type="text" class="form-control" id="recipient-name">
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Send message</button>
+      <form action="{{ route('categories.store') }}" method="POST">
+    @csrf
+    <div class="mb-3">
+        <label for="recipient-name" class="col-form-label">Categorie:</label>
+        <input type="text" class="form-control" id="recipient-name" name="name">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
       </div>
     </div>
   </div>
@@ -38,12 +51,18 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>fuyihijl</td>
-            <td>
-                <button type="button" class="btn btn-danger">Delete</button>
-            </td>
-        </tr>
+        @foreach($categories as $category)
+            <tr>
+                <td>{{ $category->name }}</td>
+                <td>
+                    <form action="{{ route('categories.destroy', $category) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
 
