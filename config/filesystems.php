af6b0c7e5f38d0f185d56e1b6d50dea7d84623d5
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-04-15 15:50:59 +0200

    Add Spatie Laravel Media Library package and update filesystem configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,6 +30,18 @@ return [
 
     'disks' => [
 
+        'media' => [
+            'driver' => 'local',
+            'root'   => public_path('media'),
+            'url'    => env('APP_URL').'/media',
+        ],
+        
+        'center_files' => [
+            'driver' => 'local',
+            'root'   => public_path('media'),
+            'url'    => env('APP_URL').'/media',
+        ],
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
--- resources/views/Admin/AddCenter.blade.php
+++ resources/views/Admin/AddCenter.blade.php
@@ -17,7 +17,8 @@
                             </div>
                         </div>
                     </div>
-
+                    <form action="{{ route('centers.store') }}" method="post" enctype="multipart/form-data">
+                                @csrf
                     <div class="row">
                         <div class="col-lg-8">
                             <div class="card">
@@ -34,15 +35,15 @@
 
                                     <div class="mb-3">
                                         <label class="form-label">Center Description</label>
-                                        <div id="ckeditor-classic">
-                                            <p>It will be as simple as occidental in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is. The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.</p>
-                                            <ul>
-                                                <li>Product Design, Figma (Software), Prototype</li>
-                                                <li>Four Dashboards : Ecommerce, Analytics, Project etc.</li>
-                                                <li>Create calendar, chat and email app pages.</li>
-                                                <li>Add authentication pages</li>
-                                            </ul>
-                                        </div>
+                                        <div class="form-floating">
+                                <textarea class="form-control" name="description" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                                <label for="floatingTextarea">description </label>
+                                </div>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label class="form-label" for="project-title-input">Adress Center</label>
+                                        <input type="text" name="adresse" class="form-control" id="project-title-input" placeholder="Enter adress Center">
                                     </div>
 
                                     <div class="row">
@@ -50,13 +51,13 @@
                                         <div class="col-lg-4">
                                         <div class="form-group">
                                         <label for="start-date">Date d'ouverture</label>
-                                        <input type="date" class="form-control" id="start-date">
+                                        <input type="date" name="overture" class="form-control" id="start-date">
                                     </div>
                                 </div>
                                         <div class="col-lg-4">
                                         <div class="form-group">
                                         <label for="end-date">Date de fermeture</label>
-                                        <input type="date" class="form-control" id="end-date">
+                                        <input type="date" name="fermeture" class="form-control" id="end-date">
                                     </div>
                                         </div>
                                     </div>
@@ -70,48 +71,11 @@
                                     <h5 class="card-title mb-0">Attached files</h5>
                                 </div>
                                 <div class="card-body">
-                                    <div>
-                                        <p class="text-muted">Add Attached files here.</p>
-
-                                        <div class="dropzone">
-                                            <div class="fallback">
-                                                <input name="file" type="file" multiple="multiple">
-                                            </div>
-                                            <div class="dz-message needsclick">
-                                                <div class="mb-3">
-                                                    <i class="display-4 text-muted ri-upload-cloud-2-fill"></i>
-                                                </div>
-
-                                                <h5>Drop files here or click to upload.</h5>
-                                            </div>
-                                        </div>
-
-                                        <ul class="list-unstyled mb-0" id="dropzone-preview">
-                                            <li class="mt-2" id="dropzone-preview-list">
-                                                <!-- This is used as the file preview template -->
-                                                <div class="border rounded">
-                                                    <div class="d-flex p-2">
-                                                        <div class="flex-shrink-0 me-3">
-                                                            <div class="avatar-sm bg-light rounded">
-                                                                <img src="#" alt="Project-Image" data-dz-thumbnail="" class="img-fluid rounded d-block">
-                                                            </div>
-                                                        </div>
-                                                        <div class="flex-grow-1">
-                                                            <div class="pt-1">
-                                                                <h5 class="fs-14 mb-1" data-dz-name="">&nbsp;</h5>
-                                                                <p class="fs-13 text-muted mb-0" data-dz-size=""></p>
-                                                                <strong class="error text-danger" data-dz-errormessage=""></strong>
-                                                            </div>
-                                                        </div>
-                                                        <div class="flex-shrink-0 ms-3">
-                                                            <button data-dz-remove="" class="btn btn-sm btn-danger">Delete</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                        <!-- end dropzon-preview -->
-                                    </div>
+                                <div class="file-loading">
+        <label class="form-label" for="file-input">Center Files</label>
+        <input type="file" name="files[]" class="form-control" id="file-input" multiple>
+    </div>
+                                       
                                 </div>
                             </div>
                             <!-- end card -->
@@ -144,11 +108,11 @@
                           <div class="card-body">
     <div class="mb-3">
         <label for="choices-categories-input" class="form-label">Categories</label>
-        <select class="form-select" data-choices="" data-choices-search-false="" id="choices-categories-input">
-            @foreach($categories as $categorie)
-                <option value="{{ $categorie->id }}">{{ $categorie->name }}</option>
-            @endforeach
-        </select>
+        <select name="category_id" class="form-select" data-choices="" data-choices-search-false="" id="choices-categories-input">
+    @foreach($categories as $categorie)
+        <option value="{{ $categorie->id }}">{{ $categorie->name }}</option>
+    @endforeach
+</select>
     </div>        
 </div>
                                 <!-- end card body -->
@@ -156,4 +120,5 @@
                         </div>
                         <!-- end col -->
                     </div>
+                    </form>
                     @endsection
