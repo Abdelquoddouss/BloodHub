a95a1ca5b2a3b325117fb29ff55f968d2f527317
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-04-16 02:54:28 +0200

    Update logout logic in AuthController.php and update user avatar in DashboardAdmin.blade.php view

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -95,7 +95,7 @@ class AuthController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
 
 
--- resources/views/Admin/DashboardAdmin.blade.php
+++ resources/views/Admin/DashboardAdmin.blade.php
@@ -305,13 +305,13 @@
                   <span class="d-flex align-items-center">
                     <img
                       class="rounded-circle header-profile-user"
-                      src="assets/img/avatar-1.jpg"
+                      src="{{ Auth::user()->getFirstMediaUrl('profile_images') }}"  
                       alt="Header Avatar"
                     />
                     <span class="text-start ms-xl-2">
                       <span
                         class="d-none d-xl-inline-block ms-1 fw-medium user-name-text"
-                        >Anna Adame</span
+                        >{{ Auth::user()->name }}</span
                       >
                       <span
                         class="d-none d-xl-block ms-1 fs-12 user-name-sub-text"
@@ -329,56 +329,15 @@
                     ></i>
                     <span class="align-middle">Profile</span></a
                   >
-                  <a class="dropdown-item" href="apps-chat.html"
-                    ><i
-                      class="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"
-                    ></i>
-                    <span class="align-middle">Messages</span></a
-                  >
-                  <a class="dropdown-item" href="apps-tasks-kanban.html"
-                    ><i
-                      class="mdi mdi-calendar-check-outline text-muted fs-16 align-middle me-1"
-                    ></i>
-                    <span class="align-middle">Taskboard</span></a
-                  >
-                  <a class="dropdown-item" href="pages-faqs.html"
-                    ><i
-                      class="mdi mdi-lifebuoy text-muted fs-16 align-middle me-1"
-                    ></i>
-                    <span class="align-middle">Help</span></a
-                  >
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="pages-profile.html"
-                    ><i
-                      class="mdi mdi-wallet text-muted fs-16 align-middle me-1"
-                    ></i>
-                    <span class="align-middle"
-                      >Balance : <b>$5971.67</b></span
-                    ></a
-                  >
-                  <a class="dropdown-item" href="pages-profile-settings.html"
-                    ><span
-                      class="badge bg-success-subtle text-success mt-1 float-end"
-                      >New</span
-                    ><i
-                      class="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"
-                    ></i>
-                    <span class="align-middle">Settings</span></a
-                  >
-                  <a class="dropdown-item" href="auth-lockscreen-basic.html"
-                    ><i
-                      class="mdi mdi-lock text-muted fs-16 align-middle me-1"
-                    ></i>
-                    <span class="align-middle">Lock screen</span></a
-                  >
-                  <a class="dropdown-item" href="auth-logout-basic.html"
-                    ><i
-                      class="mdi mdi-logout text-muted fs-16 align-middle me-1"
-                    ></i>
-                    <span class="align-middle" data-key="t-logout"
-                      >Logout</span
-                    ></a
-                  >
+                  <form action="/logout" method="POST">
+    @csrf
+    <button type="submit" class="dropdown-item">
+        <i class="mdi mdi-logout text-muted fs-16 align-middle me-1"></i>
+        <span class="align-middle" data-key="t-logout">Logout</span>
+    </button>
+</form>
+                  
                 </div>
               </div>
             </div>
