c2d10b5c7a03aa62aa55b55bae6e25cef1933da6
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-04-22 16:09:28 +0200

    Refactor print_hello_world function to greet in main.py

--- resources/views/Mails/ReservationDonSang.blade.php
+++ resources/views/Mails/ReservationDonSang.blade.php
@@ -1,44 +1,161 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <style type="text/css">
+        @media screen {
+            @font-face {
+                font-family: 'Lato';
+                font-style: normal;
+                font-weight: 400;
+                src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');
+            }
+
+            @font-face {
+                font-family: 'Lato';
+                font-style: normal;
+                font-weight: 700;
+                src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');
+            }
+
+            @font-face {
+                font-family: 'Lato';
+                font-style: italic;
+                font-weight: 400;
+                src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');
+            }
+
+            @font-face {
+                font-family: 'Lato';
+                font-style: italic;
+                font-weight: 700;
+                src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');
+            }
+        }
+
+        /* CLIENT-SPECIFIC STYLES */
+        body,
+        table,
+        td,
+        a {
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+        }
+
+        table,
+        td {
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+        }
+
+        img {
+            -ms-interpolation-mode: bicubic;
+        }
+
+        /* RESET STYLES */
+        img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+        }
+
+        table {
+            border-collapse: collapse !important;
+        }
+
         body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            color: #333;
+            height: 100% !important;
+            margin: 0 !important;
+            padding: 0 !important;
+            width: 100% !important;
         }
-        .container {
-            padding: 20px;
+
+        /* iOS BLUE LINKS */
+        a[x-apple-data-detectors] {
+            color: inherit !important;
+            text-decoration: none !important;
+            font-size: inherit !important;
+            font-family: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
         }
-        .title {
-            font-size: 24px;
-            font-weight: bold;
+
+        /* MOBILE STYLES */
+        @media screen and (max-width:600px) {
+            h1 {
+                font-size: 32px !important;
+                line-height: 32px !important;
+            }
         }
-        .content {
-            margin-top: 20px;
-        }
-        .footer {
-            margin-top: 40px;
-            font-size: 12px;
-            text-align: center;
-            color: #666;
+
+        /* ANDROID CENTER FIX */
+        div[style*="margin: 16px 0;"] {
+            margin: 0 !important;
         }
     </style>
 </head>
-<body>
-    <div class="container">
-        <div class="title">Confirmation de réservation</div>
-        <div class="content">
-            <p>Bonjour {{ Auth::user()->name }},</p>
-            <p>Votre réservation a été confirmée avec succès.</p>
-            <!-- Inclure d'autres détails de réservation si nécessaire -->
-        </div>
-        <div class="footer">
-            Merci pour votre réservation !
-        </div>
+
+<body style="background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;">
+    <!-- HIDDEN PREHEADER TEXT -->
+    <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> We're thrilled to have you here! Get ready to dive into your new account.
     </div>
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <!-- LOGO -->
+        <tr>
+            <td bgcolor="#FFA73B" align="center">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                    <tr>
+                        <td align="center" valign="top" style="padding: 40px 10px 40px 10px;"> </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#FFA73B" align="center" style="padding: 0px 10px 0px 10px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                    <tr>
+                        <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+                            <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Welcome!</h1> <img src=" https://img.icons8.com/clouds/100/000000/handshake.png" width="125" height="120" style="display: block; border: 0px;" />
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                    <tr>
+                        <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                           @if ($reservation->appointment_date)
+    <p style="margin: 10px 0;">Votre rendez-vous pour le don de sang au centre {{ $reservation->center->nom }} est prévu pour le {{ $reservation->appointment_date }}.</p>
+    <p style="margin: 10px 0;">Veuillez vous présenter à l'heure indiquée au centre de don de sang.</p>
+        <p style="margin: 10px 0;">Avant de venir, voici quelques conseils :
+                               <ul>
+                                   <li>Hydratez-vous suffisamment en buvant de l'eau.</li>
+                                   <li>Mangez un repas équilibré riche en fer.</li>
+                                   <li>Assurez-vous d'avoir eu suffisamment de sommeil la nuit précédant votre don.</li>
+                                   <li>Évitez de consommer de l'alcool au moins 24 heures avant le don.</li>
+                                   <li>Évitez les activités physiques intenses avant et après le don.</li>
+                               </ul>
+                               </p
+@else
+    <p style="margin: 10px 0;">Votre rendez-vous pour le don de sang au centre {{ $reservation->center->name }} sera planifié prochainement.</p>
+    <p style="margin: 10px 0;">Vous recevrez bientôt les détails de votre rendez-vous par e-mail.</p>
+@endif
+    
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <!-- AUTRE CONTENU DE L'EMAIL -->
+    </table>
 </body>
+
 </html>
