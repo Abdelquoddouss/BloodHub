21f83f3fdf35f55fcef91f106f40db74ac32ff1c
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-04-24 01:37:30 +0200

    Add migration to add status column to reservations table

---
+++ database/migrations/2024_04_23_230719_add_status_to_reservations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->string('status')->default('pending'); 
+        });
+    }
+    
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropColumn('status'); 
+        });
+    }
+    
+};
