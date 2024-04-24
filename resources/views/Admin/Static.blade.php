76541ba63f04a2c77a31af318cbc2740c4f1b81e
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-04-24 10:55:08 +0200

    Refactor UserController to include dashboard functionality

--- resources/views/Admin/Static.blade.php
+++ resources/views/Admin/Static.blade.php
@@ -2,148 +2,99 @@
   @section('content')
       <div class="row">
           <div class="col-xl-3 col-md-6">
-              <!-- card -->
-              <div class="card card-animate">
-                  <div class="card-body">
-                      <div class="d-flex align-items-center">
-                          <div class="flex-grow-1 overflow-hidden">
-                              <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                  Total Earnings
-                              </p>
-                          </div>
-                          <div class="flex-shrink-0">
-                              <h5 class="text-success fs-14 mb-0">
-                                  <i class="ri-arrow-right-up-line fs-13 align-middle"></i>
-                                  +16.24 %
-                              </h5>
-                          </div>
-                      </div>
-                      <div class="d-flex align-items-end justify-content-between mt-4">
-                          <div>
-                              <h4 class="fs-22 fw-semibold ff-secondary mb-4">
-                                  $<span class="counter-value" data-target="559.25">0</span>k
-                              </h4>
-                              <a href="" class="text-decoration-underline">View net earnings</a>
-                          </div>
-                          <div class="avatar-sm flex-shrink-0">
-                              <span class="avatar-title bg-success-subtle rounded fs-3">
-                                  <i class="bx bx-dollar-circle text-success"></i>
-                              </span>
-                          </div>
-                      </div>
-                  </div>
-                  <!-- end card body -->
-              </div>
-              <!-- end card -->
-          </div>
+    <div class="card card-animate">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="flex-grow-1 overflow-hidden">
+                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                        Utilisateurs Inscrits
+                    </p>
+                </div>
+            </div>
+            <div class="d-flex align-items-end justify-content-between mt-4">
+                <div>
+                    <h4 class="fs-22 fw-semibold ff-secondary mb-4">
+                        <span class="counter-value" data-target="{{ $userCount }}">0</span>
+                    </h4>
+                    <a href="" class="text-decoration-underline">Voir tous les utilisateurs</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
           <!-- end col -->
 
-          <div class="col-xl-3 col-md-6">
-              <!-- card -->
-              <div class="card card-animate">
-                  <div class="card-body">
-                      <div class="d-flex align-items-center">
-                          <div class="flex-grow-1 overflow-hidden">
-                              <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                  Orders
-                              </p>
-                          </div>
-                          <div class="flex-shrink-0">
-                              <h5 class="text-danger fs-14 mb-0">
-                                  <i class="ri-arrow-right-down-line fs-13 align-middle"></i>
-                                  -3.57 %
-                              </h5>
-                          </div>
-                      </div>
-                      <div class="d-flex align-items-end justify-content-between mt-4">
-                          <div>
-                              <h4 class="fs-22 fw-semibold ff-secondary mb-4">
-                                  <span class="counter-value" data-target="36894">0</span>
-                              </h4>
-                              <a href="" class="text-decoration-underline">View all orders</a>
-                          </div>
-                          <div class="avatar-sm flex-shrink-0">
-                              <span class="avatar-title bg-info-subtle rounded fs-3">
-                                  <i class="bx bx-shopping-bag text-info"></i>
-                              </span>
-                          </div>
-                      </div>
-                  </div>
-                  <!-- end card body -->
-              </div>
-              <!-- end card -->
-          </div>
+        <div class="col-xl-3 col-md-6">
+    <div class="card card-animate">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="flex-grow-1 overflow-hidden">
+                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                        Centres
+                    </p>
+                </div>
+            </div>
+            <div class="d-flex align-items-end justify-content-between mt-4">
+                <div>
+                    <h4 class="fs-22 fw-semibold ff-secondary mb-4">
+                        <span class="counter-value" data-target="{{ $centerCount }}">0</span>
+                    </h4>
+                    <a href="" class="text-decoration-underline">Voir tous les centres</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
           <!-- end col -->
 
-          <div class="col-xl-3 col-md-6">
-              <!-- card -->
-              <div class="card card-animate">
-                  <div class="card-body">
-                      <div class="d-flex align-items-center">
-                          <div class="flex-grow-1 overflow-hidden">
-                              <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                  Customers
-                              </p>
-                          </div>
-                          <div class="flex-shrink-0">
-                              <h5 class="text-success fs-14 mb-0">
-                                  <i class="ri-arrow-right-up-line fs-13 align-middle"></i>
-                                  +29.08 %
-                              </h5>
-                          </div>
-                      </div>
-                      <div class="d-flex align-items-end justify-content-between mt-4">
-                          <div>
-                              <h4 class="fs-22 fw-semibold ff-secondary mb-4">
-                                  <span class="counter-value" data-target="183.35">0</span>M
-                              </h4>
-                              <a href="" class="text-decoration-underline">See details</a>
-                          </div>
-                          <div class="avatar-sm flex-shrink-0">
-                              <span class="avatar-title bg-warning-subtle rounded fs-3">
-                                  <i class="bx bx-user-circle text-warning"></i>
-                              </span>
-                          </div>
-                      </div>
-                  </div>
-                  <!-- end card body -->
-              </div>
-              <!-- end card -->
-          </div>
+         <div class="col-xl-3 col-md-6">
+    <div class="card card-animate">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="flex-grow-1 overflow-hidden">
+                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                        Réservations Passées
+                    </p>
+                </div>
+            </div>
+            <div class="d-flex align-items-end justify-content-between mt-4">
+                <div>
+                    <h4 class="fs-22 fw-semibold ff-secondary mb-4">
+                        <span class="counter-value" data-target="{{ $reservationCount }}">0</span>
+                    </h4>
+                    <a href="" class="text-decoration-underline">Voir toutes les réservations</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
           <!-- end col -->
 
-          <div class="col-xl-3 col-md-6">
-              <!-- card -->
-              <div class="card card-animate">
-                  <div class="card-body">
-                      <div class="d-flex align-items-center">
-                          <div class="flex-grow-1 overflow-hidden">
-                              <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                  My Balance
-                              </p>
-                          </div>
-                          <div class="flex-shrink-0">
-                              <h5 class="text-muted fs-14 mb-0">+0.00 %</h5>
-                          </div>
-                      </div>
-                      <div class="d-flex align-items-end justify-content-between mt-4">
-                          <div>
-                              <h4 class="fs-22 fw-semibold ff-secondary mb-4">
-                                  $<span class="counter-value" data-target="165.89">0</span>k
-                              </h4>
-                              <a href="" class="text-decoration-underline">Withdraw money</a>
-                          </div>
-                          <div class="avatar-sm flex-shrink-0">
-                              <span class="avatar-title bg-primary-subtle rounded fs-3">
-                                  <i class="bx bx-wallet text-primary"></i>
-                              </span>
-                          </div>
-                      </div>
-                  </div>
-                  <!-- end card body -->
-              </div>
-              <!-- end card -->
-          </div>
+       <div class="col-xl-3 col-md-6">
+    <div class="card card-animate">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="flex-grow-1 overflow-hidden">
+                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                        Utilisateurs ayant Passé le Test
+                    </p>
+                </div>
+            </div>
+            <div class="d-flex align-items-end justify-content-between mt-4">
+                <div>
+                    <h4 class="fs-22 fw-semibold ff-secondary mb-4">
+                        <span class="counter-value" data-target="{{ $testPassCount }}">0</span>
+                    </h4>
+                    <a href="" class="text-decoration-underline">Voir tous les utilisateurs ayant passé le test</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
           <!-- end col -->
       </div>
 
@@ -206,8 +157,5 @@
               </div>
           </div>
       </div>
-
-
-
       </div>
   @endsection
